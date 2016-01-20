0145dae94832987f9523aab399a7f1c6ef6ff88b
jkalista <jyri.kalistaja@helsinki.fi>
2016-01-21 00:10:29 +0200

    projekti ja gitignore

---
+++ Avaruuspuolustus/src/main/java/Avaruuspuolustus/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Avaruuspuolustus;
+
+/**
+ *
+ * @author Jyri
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.print("Aloitellaan ohjelmointi seuraavalla viikolla :D");
+    }
+    
+    
+}
